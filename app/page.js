bc3a35cb30016d8cff64f346e4b9a281ed2b2d81
Andriikryl <andreyk557@gmail.com>
2023-06-08 20:53:54 +0300

    add data from strip

--- app/page.js
+++ app/page.js
@@ -1,4 +1,19 @@
-export default function Home() {
+import Stripe from "stripe";
+
+async function getStripeProducts() {
+  const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
+    apiVersion: "2020-08-27",
+  });
+  const res = await stripe.prices.list({
+    expand: ["data.product"],
+  });
+  const prices = res.data;
+  return prices;
+}
+
+export default async function Home() {
+  const products = await getStripeProducts();
+  console.log(products);
   return (
     <main>
       <h1>sda</h1>
