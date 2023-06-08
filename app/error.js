506c2f245dde0c5ceb7f5ee847be173b35e1d922
Andriikryl <andreyk557@gmail.com>
2023-06-08 21:15:08 +0300

    add maping product card

---
+++ app/error.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div>
+      <h1>Errror............... bymmer</h1>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Stripe from "stripe";
+import ProductCard from "./ProductCard";
 
 async function getStripeProducts() {
   const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
@@ -15,8 +16,12 @@ export default async function Home() {
   const products = await getStripeProducts();
   console.log(products);
   return (
-    <main>
-      <h1>sda</h1>
+    <main className="p-4 flex flex-col">
+      <div className="max-w-[1000px] w-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+        {products.map((product, productIndex) => {
+          return <ProductCard key={productIndex} product={product} />;
+        })}
+      </div>
     </main>
   );
 }
